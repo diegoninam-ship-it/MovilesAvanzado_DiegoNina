0ad5765c59be7010eb93e3d2746cf6f79caa2068
Diego Nina <diego.nina.m@tecsup.edu.pe>
2026-09-02 09:53:39 -0500

    Calculos

--- Semana02/Actividad02.playground/Contents.swift
+++ Semana02/Actividad02.playground/Contents.swift
@@ -14,6 +14,33 @@ let montoTotal = precioUnitario * cantidad
 
 
 
+while true {
+    print("Elige el plan de pago (6, 12, 24) en meses: ")
+    let meses: Int = Int(readLine() ?? "0") ?? 0
+
+    if meses == 6 || meses == 12 || meses == 24 {
+        let tasaInteres: Double
+        switch meses {
+        case 6:
+            tasaInteres = 0.2
+        case 12:
+            tasaInteres = 0.4
+        default:
+            tasaInteres = 0.6
+        }
+
+        let interes = montoTotal * tasaInteres
+        let montoFinal = montoTotal + interes
+        let pagoMensual = montoFinal / Double(meses)
+        
+        
+        break
+    } else {
+        print("Opción inválida. Intente de nuevo.")
+    }
+}
+
+
     
 
 
