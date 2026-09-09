76b393408977852730ce7d67951d91d3aade19f6
diego.nina.@tecsup.edu.pe <diego.nina.m@tecsup.edu.pe>
2026-09-09 10:29:06 -0500

    feat: Upload ejercicio 01

---
+++ Semana04/Ejercicio01/Ejercicio01/Ejercicio01App.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio01App.swift
+//  Ejercicio01
+//
+//  Created by Tecsup on 9/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
