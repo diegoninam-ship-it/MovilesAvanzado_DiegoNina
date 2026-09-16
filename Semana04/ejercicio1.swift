c3eef0b65219addb0fc684237138f542357a1c2f
diegoninam-ship-i <diego.nina.m@tecsup.edu.pe>
2026-09-15 20:55:33 -0500

    feat: agregar SucursalLima y SucursalOutlet con override de descuento y envio

---
+++ Semana04/ejercicio1.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+enum CategoriaElectro {
+    case lineaBlanca, tecnologia, pequenos
+}
+
+struct Electrodomestico {
+    let nombre: String
+    let marca: String
+    let precioLista: Double
+    let categoria: CategoriaElectro
+}
+
+class SucursalLima: Sucursal {
+    override func descuento() -> Double {
+        return 0.10
+    }
+
+    override func costoEnvio(monto: Double) -> Double {
+        if monto >= 1500 {
+            return 0.0
+        } else {
+            return 30.0
+        }
+    }
+}
+
+class SucursalOutlet: Sucursal {
+    override func descuento() -> Double {
+        return 0.25
+    }
+
+    override func costoEnvio(monto: Double) -> Double {
+        return 0.0
+    }
+}
